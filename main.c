2a33ba10f63255e6733cac06897a647543489696
Tom O'Brien <tomobrien@Toms-MacBook-Pro.local>
2017-04-14 17:28:34 +0100

    More opcodes

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "opcodes.h"
 
 typedef struct _number _register;
@@ -57,7 +58,16 @@ int main(int argc, char *argv[]) {
         // TODO: exit codes
         return 1;
     }
-    _register r0, r1, r2, r3, r4, r5, r6, r7;
+    uint16_t r0, r1, r2, r3, r4, r5, r6, r7;
+
+    r0 = 32768;
+    r1 = 32769;
+    r2 = 32770;
+    r3 = 32771;
+    r4 = 32772;
+    r5 = 32773;
+    r6 = 32774;
+    r7 = 32775;
 
     val* memory = init_memory();
     struct stack* stack = init_stack();
@@ -72,24 +82,40 @@ int main(int argc, char *argv[]) {
 
     int pc = 0;
     uint16_t foo;
-    while (memory[pc] != 0) {
+    uint16_t bar;
+    bool debug = true;
+    while (true) {
         val low_byte = memory[pc];
         val high_byte = memory[pc + 1];
 
         uint16_t v = low_byte + (((uint16_t)  high_byte) << 8);
 
         switch (v) {
-            case OPCODE_HALT:
+            case OPCODE_HALT: // 0
                 return 0;
-            case OPCODE_OUT:
-                foo = (uint16_t) memory[pc + 2] + (((uint16_t) memory[pc + 2]) << 8);
+            case OPCODE_JMP: // 6
+                foo = (uint16_t) memory[pc + 2] + (((uint16_t) memory[pc + 3]) << 8);
+                pc = 2 * foo;
+                break;
+            case OPCODE_JT: // 7
+                foo = (uint16_t) memory[pc + 2] + (((uint16_t) memory[pc + 3]) << 8);
+                bar = (uint16_t) memory[pc + 4] + (((uint16_t) memory[pc + 5]) << 8);
+                if (foo != 0) {
+                    pc = 2 * bar;
+                } else {
+                    pc = pc + 6;
+                }
+                break;
+            case OPCODE_OUT: // 19
+                foo = (uint16_t) memory[pc + 2] + (((uint16_t) memory[pc + 3]) << 8);
                 printf("%c", foo);
                 pc = pc + 4;
                 break;
-            case OPCODE_NOOP:
+            case OPCODE_NOOP: // 21
                 pc = pc + 2;
                 break;
             default:
+                printf("NOT IMPLEMENTED: %d\n", v);
                 return 1;
         }
     }
