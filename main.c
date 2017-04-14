a52d8b1062d42a704bde2afd097250ad8cc3545f
Tom O'Brien <tomobrien@Toms-MacBook-Pro.local>
2017-04-14 18:51:28 +0100

    more opcodes

--- main.c
+++ main.c
@@ -4,13 +4,8 @@
 #include <stdbool.h>
 #include "opcodes.h"
 
-typedef struct _number _register;
 typedef uint8_t val;
-
-struct _number {
-    uint8_t low;
-    uint8_t high;
-};
+typedef uint16_t address;
 
 struct stack {
     val value;
@@ -52,8 +47,12 @@ void free_memory(val* memory) {
     free(memory);
 }
 
-uint16_t uint16_at(val* memory, uint16_t address) {
-    return (uint16_t) memory[address] + (((uint16_t) memory[address + 1]) << 8);
+uint16_t uint16_at(val* memory, address address) {
+    uint16_t val = (uint16_t) memory[address] + (((uint16_t) memory[address + 1]) << 8);
+    if (val >= 0x8000)
+        val = (uint16_t) memory[val] + (((uint16_t) memory[val + 1]) << 8);
+
+    return val;
 }
 
 int main(int argc, char *argv[]) {
@@ -61,72 +60,73 @@ int main(int argc, char *argv[]) {
         // TODO: exit codes
         return 1;
     }
-    uint16_t r0, r1, r2, r3, r4, r5, r6, r7;
-
-    r0 = 32768;
-    r1 = 32769;
-    r2 = 32770;
-    r3 = 32771;
-    r4 = 32772;
-    r5 = 32773;
-    r6 = 32774;
-    r7 = 32775;
 
     val* memory = init_memory();
     struct stack* stack = init_stack();
 
     // Load program into memory.
-    printf("%s\n", argv[1]);
     FILE *fp;
     fp = fopen(argv[1], "r");
-
     fread(memory, sizeof(val), 1 << 16, fp);
     fclose(fp);
 
-    int pc = 0;
+    printf("%x\t%x\t%x\t%x\t%x\t%x\t%x\t%x\n",
+           uint16_at(memory, 0x8000),
+           uint16_at(memory, 0x8001),
+           uint16_at(memory, 0x8002),
+           uint16_at(memory, 0x8003),
+           uint16_at(memory, 0x8004),
+           uint16_at(memory, 0x8005),
+           uint16_at(memory, 0x8006),
+           uint16_at(memory, 0x8007));
+
+    address pc = 0;
     // TODO: these should be pushed onto a stack.
     uint16_t a;
     uint16_t b;
-    bool debug = true;
     while (true) {
-        val low_byte = memory[pc];
-        val high_byte = memory[pc + 1];
-
-        uint16_t v = low_byte + (((uint16_t)  high_byte) << 8);
+        uint16_t v = uint16_at(memory, pc);
 
         switch (v) {
             case OPCODE_HALT: // 0
+                free_memory(memory);
+
                 return 0;
             case OPCODE_JMP: // 6
-                a = uint16_at(memory, pc + 2);
-                pc = 2 * a;
+                a = uint16_at(memory, pc + (address) 2);
+                pc = (address) 2 * a;
                 break;
             case OPCODE_JT: // 7
-                a = uint16_at(memory, pc + 2);
-                b = uint16_at(memory, pc + 4);
+                a = uint16_at(memory, pc + (address) 2);
+                b = uint16_at(memory, pc + (address) 4);
+
                 if (a != 0) {
-                    pc = 2 * b;
+                    pc = (address) 2 * b;
                 } else {
-                    pc = pc + 6;
+                    pc = pc + (address) 6;
+                }
+                break;
+            case OPCODE_JF: // 8
+                a = uint16_at(memory, pc + (address) 2);
+                b = uint16_at(memory, pc + (address) 4);
+
+                if (a == 0) {
+                    pc = (address) 2 * b;
+                } else {
+                    pc = pc + (address) 6;
                 }
                 break;
             case OPCODE_OUT: // 19
-                a = uint16_at(memory, pc + 2);
+                a = uint16_at(memory, pc + (address) 2);
                 printf("%c", a);
-                pc = pc + 4;
+                pc = pc + (address) 4;
                 break;
             case OPCODE_NOOP: // 21
-                pc = pc + 2;
+                pc = pc + (address) 2;
                 break;
             default:
                 printf("NOT IMPLEMENTED: %d\n", v);
                 return 1;
         }
     }
-
-
-    // TODO: free_stack ?
-    free_memory(memory);
-
-    return 0;
 }
