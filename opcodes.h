17f9ffcb3bd3a28224aae3b5447d057ef6b1bf8b
Tom O'Brien <tomobrien@Toms-MacBook-Pro.local>
2017-04-14 17:12:01 +0100

    Opcodes

---
+++ opcodes.h
@@ -0,0 +1,31 @@
+//
+// Created by Tom O'Brien on 14/04/2017.
+//
+
+#ifndef SYNACORE_OPCODES_H
+#define SYNACORE_OPCODES_H
+
+#define OPCODE_HALT 0
+#define OPCODE_SET 1
+#define OPCODE_PUSH 2
+#define OPCODE_POP 3
+#define OPCODE_EQ 4
+#define OPCODE_GT 5
+#define OPCODE_JMP 6
+#define OPCODE_JT 7
+#define OPCODE_JF 8
+#define OPCODE_ADD 9
+#define OPCODE_MULT 10
+#define OPCODE_MOD 11
+#define OPCODE_AND 12
+#define OPCODE_OR 13
+#define OPCODE_NOT 14
+#define OPCODE_RMEM 15
+#define OPCODE_WMEM 16
+#define OPCODE_CALL 17
+#define OPCODE_RET 18
+#define OPCODE_OUT 19
+#define OPCODE_IN 20
+#define OPCODE_NOOP 21
+
+#endif //SYNACORE_OPCODES_H
